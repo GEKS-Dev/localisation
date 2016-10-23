5e66a28f4e56164d49bc34e1a1e514b44bf63a1d
RusFjord <teocharmillion@gmail.com>
2016-10-23 11:45:50 +0300

    Структура сайта: шаблоны страниц и контроллеры

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ app.use(express.static('static'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', require('./controllers/home'));
+app.get('/', require('./controllers/home').get);
+app.get('/login', require('./controllers/login').get);
+app.get('/translate', require('./controllers/translate').get);
+app.get('/inspection', require('./controllers/inspection').get);
+app.get('/upload', require('./controllers/upload').get);
+app.get('/download', require('./controllers/download').get);
 app.get('*', function (req, res) {
     res.send("404 - This page not found!!!");
 });
